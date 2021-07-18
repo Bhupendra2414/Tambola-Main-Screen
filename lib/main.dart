103ffb7065600628fa60f327d4933ca3d4ec600c
Bhupendra Singh Sisodiya <bhupendrasingh.bh2001@gmail.com>
2021-07-18 20:54:44 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -28,39 +28,63 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
+  bool _hasBeenPressed = false;
+
   Widget build(BuildContext context) {
+    List<Widget> myRowChildren = [];
+    List<List<Widget>> numbers = [];
+    List<Widget> columnNumbers = [];
+    int z = 0;
+    for (int i = 1; i <= 9; i++) {
+      int maxColNr = 10;
+      for (int y = 1; y <= maxColNr; y++) {
+        int currentNumber = z + y; // 0,1,2,3,4,5,6,7,8,9,10, 10,11, 12, 13,14
+        columnNumbers.add(Container(
+          height: 40,
+          width: 35,
+          //color: _hasBeenPressed ? Colors.black : Colors.greenAccent,
+          child: TextButton(
+              child: Text('$currentNumber'),
+              style: TextButton.styleFrom(
+                primary: _hasBeenPressed ? Colors.black : Colors.blue,
+              ),
+              onPressed: () {
+                setState(() {
+                  _hasBeenPressed = !_hasBeenPressed;
+                  // _hasBeenPressed = false;
+                });
+              }),
+        ));
+      }
+      z += maxColNr;
+      numbers.add(columnNumbers);
+      columnNumbers = [];
+    }
+
+    myRowChildren = numbers
+        .map(
+          (columns) => Card(
+            child: Wrap(
+              // mainAxisAlignment: MainAxisAlignment.center,
+              direction: Axis.vertical,
+
+              children: columns.map((n) {
+                return Container(
+                  child: n,
+                );
+              }).toList(),
+            ),
+          ),
+        )
+        .toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: SingleChildScrollView(
-        child: Container(
-          child: Column(
-            children: <Widget>[
-              Table(
-                  border: TableBorder.all(color: Colors.black),
-                  columnWidths: {
-                    0: FixedColumnWidth(100.0),
-                    1: FixedColumnWidth(100.0),
-                    2: FixedColumnWidth(100.0),
-                  },
-                  children: [
-                    for (int i = 1; i <= 90; i++)
-                      TableRow(children: [
-                        Column(
-                          children: [
-                            TextButton(
-                                child: Text('$i'),
-                                onPressed: () {
-                                  print('Enter');
-                                }),
-                          ],
-                        )
-                      ])
-                  ])
-            ],
-          ),
-        ),
+      body: Row(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        children: myRowChildren,
       ),
 
       // This trailing comma makes auto-formatting nicer for build methods.
